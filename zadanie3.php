0645d6d3c373d4b2255f386021dda9dfc7d068ae
GrubaRybaXD <GrubaRybaXD@onet.pl>
2021-03-31 13:29:49 +0200

    Dadanie sprawdzenia zadanie3.php

--- zadanie3.php
+++ zadanie3.php
@@ -4,6 +4,11 @@ $port = '3306';
 $user = 'root';
 $passwd = '';
 $db = 'imie';
- $pdo = new PDO('mysql:host=' . $host . ';dbname=' . $db . ';port=' . $port . ";charset=utf8", $user, $passwd );
-
+ 
+try{
+   $pdo = new PDO('mysql:host=' . $host . ';dbname=' . $db . ';port=' . $port . ";charset=utf8", $user, $passwd );
+    echo('połączono z bazą danych');
+}catch(PDOException $e){
+    echo('Problem z połączeniem');
+}
 ?>
