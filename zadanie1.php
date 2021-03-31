24e295b7b8b699e9b406864e7e8cc32f36e42d4c
GrubaRybaXD <GrubaRybaXD@onet.pl>
2021-03-31 13:18:08 +0200

    Dodanie sprawdzenia do zadanie1.php

--- zadanie1.php
+++ zadanie1.php
@@ -3,4 +3,9 @@
 $connect = mysqli_connect("localhost","user","passwd","imie");
 
 
+if (mysqli_connect_errno()) {
+  echo "Nie udało się połączyć z bazą danych " . mysqli_connect_error();
+}else{
+ echo "Połączono z bazą danych";
+}
 ?>
